277db8b5a90c07c9b28f47acab17745b579dd97a
furretcat <przygoda.alex@gmail.com>
2023-04-14 11:10:49 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main extends Application {
+
+    Stage stage;
+
+    public static List<String> moods = new ArrayList<>();
+
+    public static ComboBox<String> moodPicker = new ComboBox<>();
+
+    public static TextField timeTextfield = new TextField();
+
+    public static Button button = new Button();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        moods.add("Awesome");
+        moods.add("Happy");
+        moods.add("Alright");
+        moods.add("Bad");
+        moods.add("Awful");
+
+        moodPicker.getItems().addAll(Main.moods);
+        moodPicker.setOnAction(event -> {
+            String chosen = moodPicker.getValue();
+        });
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainLayout.fxml"));
+        primaryStage.setScene(new Scene(root));
+
+        stage = primaryStage;
+        primaryStage.setTitle("Coffee");
+        primaryStage.show();
+
+    }
+
+
+}
+
